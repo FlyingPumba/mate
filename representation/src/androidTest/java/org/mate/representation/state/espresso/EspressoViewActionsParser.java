6c6fa0e6d0d8fd6e7d4a021ff149fe7c2ef8a767
Ivan <iarcuschin@gmail.com>
2022-07-12 14:16:09 -0300

    Only perform closeSoftKeyboard if keyboard is opened

--- representation/src/androidTest/java/org/mate/representation/state/espresso/EspressoViewActionsParser.java
+++ representation/src/androidTest/java/org/mate/representation/state/espresso/EspressoViewActionsParser.java
@@ -1,5 +1,7 @@
 package org.mate.representation.state.espresso;
 
+import androidx.annotation.NonNull;
+
 import org.mate.commons.interaction.action.espresso.EspressoView;
 import org.mate.commons.interaction.action.espresso.actions.BackAction;
 import org.mate.commons.interaction.action.espresso.actions.ClearTextAction;
@@ -16,9 +18,11 @@ import org.mate.commons.interaction.action.espresso.actions.SwipeLeftAction;
 import org.mate.commons.interaction.action.espresso.actions.SwipeRightAction;
 import org.mate.commons.interaction.action.espresso.actions.SwipeUpAction;
 import org.mate.commons.interaction.action.espresso.actions.TypeTextAction;
+import org.mate.representation.DeviceInfo;
 import org.mate.representation.input_generation.TextDataGenerator;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -46,25 +50,7 @@ public class EspressoViewActionsParser {
         // There are many actions that we can possible perform on a View (e.g., Click, ClearText,
         // etc). To determine if an action is valid for a View, we check that the later matches the
         // constraints imposed by the actual Espresso's ViewAction.
-        EspressoViewAction[] possibleActions = {
-            new BackAction(),
-            new ClearTextAction(),
-            new ClickAction(),
-            new CloseSoftKeyboardAction(),
-            // Double click actions are disabled for now, since they tend to lead to flaky tests
-            // new DoubleClickAction(),
-            new EnterAction(),
-            new LongClickAction(),
-            new MenuAction(),
-            new PressIMEAction(),
-            new ScrollToAction(),
-            new SwipeDownAction(),
-            new SwipeLeftAction(),
-            new SwipeRightAction(),
-            new SwipeUpAction(),
-            // We use empty text for the TypeTextAction until we know if we can use it for this view
-            new TypeTextAction("")
-        };
+        List<EspressoViewAction> possibleActions = getAvailableActions();
 
         for (EspressoViewAction action : possibleActions) {
             if (action.isValidForView(espressoView.getView())) {
@@ -80,5 +66,35 @@ public class EspressoViewActionsParser {
         }
 
         return parsedActions;
+    }
+
+    @NonNull
+    private List<EspressoViewAction> getAvailableActions() {
+        EspressoViewAction[] possibleActions = {
+            new BackAction(),
+            new ClearTextAction(),
+            new ClickAction(),
+            // Double click actions are disabled for now, since they tend to lead to flaky tests
+            // new DoubleClickAction(),
+            new EnterAction(),
+            new LongClickAction(),
+            new MenuAction(),
+            new PressIMEAction(),
+            new ScrollToAction(),
+            new SwipeDownAction(),
+            new SwipeLeftAction(),
+            new SwipeRightAction(),
+            new SwipeUpAction(),
+            // We use empty text for the TypeTextAction until we know if we can use it for this view
+            new TypeTextAction("")
+        };
+
+        List<EspressoViewAction> result = Arrays.asList(possibleActions);
+
+        if (DeviceInfo.getInstance().isKeyboardOpened()) {
+            result.add(new CloseSoftKeyboardAction());
+        }
+
+        return result;
     }
 }
